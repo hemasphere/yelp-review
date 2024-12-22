4cd5fd2a95d3a5290af11cf84bebab44dfe8c826
Hema Muni <hemamuni@Hemas-MacBook-Pro-2.local>
2024-12-22 12:22:12 -0500

    Check to see if expected first 5 zipcodes are the same as the results first 5 zipcodes

--- src/test/scala/com/turtles/yelp/parser/DataParserSpec.scala
+++ src/test/scala/com/turtles/yelp/parser/DataParserSpec.scala
@@ -10,4 +10,17 @@ class DataParserSpec extends AnyFreeSpec with Matchers {
   "DataParser sad case" in {
     DataParser.aggregateZipCodes("/yelp_academic_dataset_business_small_error.json") should be(Nil)
   }
+
+  "DataParser first 5 zip codes" in {
+    // List the expected first 5 zip codes
+    val expectedZipCodes = List("93101", "19124", "19047", "85742", "19406")
+    println("Expected: " + expectedZipCodes)
+
+    // Aggregate zip codes from the JSON file
+    val resultZipCodes = DataParser.aggregateZipCodes("/yelp_academic_dataset_business.json")
+    println("Results: " + resultZipCodes.take(5))
+
+    // Check if the first 5 zip codes match the expected values
+    resultZipCodes.take(5) should equal(expectedZipCodes)
+  }
 }
